e71426f6e85873c0898dba7e9b7737ea1cc8b5ef
LeShadow <sepr@opsec.be>
2024-12-16 14:17:00 +0100

    check if input is path

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def build_languages_list(languages):
 
 def detect_extensions():
     changed_files_data = changed_files
-    if 'added_files.txt' in changed_files:
+    if os.path.isfile(changed_files):
         with open(changed_files) as f:
             changed_files_data = f.read()
     changed_files_list = changed_files_data.split(',')
